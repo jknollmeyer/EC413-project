024eba300d8d25caa535ee4141bfcfa863ebb219
Knollmeyer <jknollm@signals35.bu.edu>
2016-04-19 18:32:03 -0400

    Correct wire width for PCWriteSourceMux

--- PCSourceMux.v
+++ PCSourceMux.v
@@ -18,18 +18,24 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module PCSourceMux(
+module PCWriteSourceMux(
 	input[1:0] PCSource,
 	input[DATA_WIDTH-1:0] ALUOut,
 	input[DATA_WIDTH-1:0] ALURegisterOut,
 	input[25:0] Immediate,
-	output[DATA_WIDTH-1:0] out
+	output[ADDR_WIDTH-1:0] out
     );
 	
 	parameter DATA_WIDTH = 32;
+	parameter ADDR_WIDTH = 16;
 	
-	assign out = PCSource[1] && ~PCSource[0] ? Immediate :
-					 ~PCSource[1] && PCSource[0] ? ALURegisterOut :
-					 ~PCSource[1] && ~PCSource[1] ? ALUOut : -1;
+	// Use ternary operator to assign outputs to wires
+	// 10: PCWriteSource = Instruction Immediate
+	// 01: PCWriteSource = ALU Register Output
+   // 00: PCWriteSource = ALU Output
+	
+	assign out = PCSource[1] && ~PCSource[0] ? Immediate[15:0] :
+					 ~PCSource[1] && PCSource[0] ? ALURegisterOut[15:0] :
+					 ~PCSource[1] && ~PCSource[1] ? ALUOut[15:0] : -1;
 
 endmodule
